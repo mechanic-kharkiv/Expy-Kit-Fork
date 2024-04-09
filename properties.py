2d5990b6061616cb09e2104b71111ed5d894de42
mechanic <28224623+mechanic-kharkiv@users.noreply.github.com>
2024-04-09 07:26:41 +0300

    using _order in RetargetBase._as_dict();

--- properties.py
+++ properties.py
@@ -6,6 +6,7 @@ from bpy.props import BoolProperty
 from bpy.props import EnumProperty
 
 from . import preset_handler
+from .utils import make_annotations
 
 
 class RetargetBase():
@@ -24,7 +25,11 @@ class RetargetBase():
     def _as_dict(self, base=''):
         "returns all props with dot-delimited keys"
         _res = {}
-        for _attr in self.rna_type.properties.keys():
+        if hasattr(self, "_order"):
+            _items = self._order
+        else:
+            _items = self.rna_type.properties.keys()
+        for _attr in _items:
             if _attr in ("rna_type", "name") or not hasattr(self, _attr):
                 continue
             _prop = getattr(self, _attr)
@@ -61,87 +66,94 @@ class RetargetBase():
             setattr(_prop, _attrs[-1], v)
 
 
+@make_annotations
 class RetargetSpine(RetargetBase, PropertyGroup):
-    head: StringProperty(name="head")
-    neck: StringProperty(name="neck")
-    spine2: StringProperty(name="spine2")
-    spine1: StringProperty(name="spine1")
-    spine: StringProperty(name="spine")
-    hips: StringProperty(name="hips")
+    head = StringProperty(name="head")
+    neck = StringProperty(name="neck")
+    spine2 = StringProperty(name="spine2")
+    spine1 = StringProperty(name="spine1")
+    spine = StringProperty(name="spine")
+    hips = StringProperty(name="hips")
 
 
+@make_annotations
 class RetargetArm(RetargetBase, PropertyGroup):
-    shoulder: StringProperty(name="shoulder")
-    arm: StringProperty(name="arm")
-    arm_twist: StringProperty(name="arm_twist")
-    arm_twist_02: StringProperty(name="arm_twist_02")
-    forearm: StringProperty(name="forearm")
-    forearm_twist: StringProperty(name="forearm_twist")
-    forearm_twist_02: StringProperty(name="forearm_twist_02")
-    hand: StringProperty(name="hand")
+    shoulder = StringProperty(name="shoulder")
+    arm = StringProperty(name="arm")
+    arm_twist = StringProperty(name="arm_twist")
+    arm_twist_02 = StringProperty(name="arm_twist_02")
+    forearm = StringProperty(name="forearm")
+    forearm_twist = StringProperty(name="forearm_twist")
+    forearm_twist_02 = StringProperty(name="forearm_twist_02")
+    hand = StringProperty(name="hand")
 
-    name: StringProperty(default='arm')
+    name = StringProperty(default='arm')
 
 
+@make_annotations
 class RetargetLeg(RetargetBase, PropertyGroup):
-    upleg: StringProperty(name="upleg")
-    upleg_twist: StringProperty(name="upleg_twist")
-    upleg_twist_02: StringProperty(name="upleg_twist_02")
-    leg: StringProperty(name="leg")
-    leg_twist: StringProperty(name="leg_twist")
-    leg_twist_02: StringProperty(name="leg_twist_02")
-    foot: StringProperty(name="foot")
-    toe: StringProperty(name="toe")
+    upleg = StringProperty(name="upleg")
+    upleg_twist = StringProperty(name="upleg_twist")
+    upleg_twist_02 = StringProperty(name="upleg_twist_02")
+    leg = StringProperty(name="leg")
+    leg_twist = StringProperty(name="leg_twist")
+    leg_twist_02 = StringProperty(name="leg_twist_02")
+    foot = StringProperty(name="foot")
+    toe = StringProperty(name="toe")
 
-    name: StringProperty(default='leg')
+    name = StringProperty(default='leg')
 
 
+@make_annotations
 class RetargetFinger(RetargetBase, PropertyGroup):
-    meta: StringProperty(name="meta")
-    a: StringProperty(name="A")
-    b: StringProperty(name="B")
-    c: StringProperty(name="C")
+    meta = StringProperty(name="meta")
+    a = StringProperty(name="A")
+    b = StringProperty(name="B")
+    c = StringProperty(name="C")
 
 
+@make_annotations
 class RetargetFingers(RetargetBase, PropertyGroup):
-    thumb: PointerProperty(type=RetargetFinger)
-    index: PointerProperty(type=RetargetFinger)
-    middle: PointerProperty(type=RetargetFinger)
-    ring: PointerProperty(type=RetargetFinger)
-    pinky: PointerProperty(type=RetargetFinger)
+    thumb = PointerProperty(type=RetargetFinger)
+    index = PointerProperty(type=RetargetFinger)
+    middle = PointerProperty(type=RetargetFinger)
+    ring = PointerProperty(type=RetargetFinger)
+    pinky = PointerProperty(type=RetargetFinger)
 
-    name: StringProperty(default='fingers')
+    name = StringProperty(default='fingers')
 
 
+@make_annotations
 class RetargetFaceSimple(RetargetBase, PropertyGroup):
-    jaw: StringProperty(name="jaw")
-    left_eye: StringProperty(name="left_eye")
-    right_eye: StringProperty(name="right_eye")
+    jaw = StringProperty(name="jaw")
+    left_eye = StringProperty(name="left_eye")
+    right_eye = StringProperty(name="right_eye")
 
-    left_upLid: StringProperty(name="left_upLid")
-    right_upLid: StringProperty(name="right_upLid")
+    left_upLid = StringProperty(name="left_upLid")
+    right_upLid = StringProperty(name="right_upLid")
 
-    super_copy: BoolProperty(default=True)
+    super_copy = BoolProperty(default=True)
 
 
+@make_annotations
 class RetargetSettings(RetargetBase, PropertyGroup):
-    face: PointerProperty(type=RetargetFaceSimple)
-    spine: PointerProperty(type=RetargetSpine)
+    face = PointerProperty(type=RetargetFaceSimple)
+    spine = PointerProperty(type=RetargetSpine)
 
-    left_arm: PointerProperty(type=RetargetArm)
-    left_arm_ik: PointerProperty(type=RetargetArm)
-    left_fingers: PointerProperty(type=RetargetFingers)
+    left_arm = PointerProperty(type=RetargetArm)
+    left_arm_ik = PointerProperty(type=RetargetArm)
+    left_fingers = PointerProperty(type=RetargetFingers)
 
-    right_arm: PointerProperty(type=RetargetArm)
-    right_arm_ik: PointerProperty(type=RetargetArm)
-    right_fingers: PointerProperty(type=RetargetFingers)
+    right_arm = PointerProperty(type=RetargetArm)
+    right_arm_ik = PointerProperty(type=RetargetArm)
+    right_fingers = PointerProperty(type=RetargetFingers)
 
-    left_leg: PointerProperty(type=RetargetLeg)
-    left_leg_ik: PointerProperty(type=RetargetLeg)
-    right_leg: PointerProperty(type=RetargetLeg)
-    right_leg_ik: PointerProperty(type=RetargetLeg)
+    left_leg = PointerProperty(type=RetargetLeg)
+    left_leg_ik = PointerProperty(type=RetargetLeg)
+    right_leg = PointerProperty(type=RetargetLeg)
+    right_leg_ik = PointerProperty(type=RetargetLeg)
 
-    root: StringProperty(name="root")
+    root = StringProperty(name="root")
 
     def has_settings(self):
         for setting in (self.spine, self.left_arm, self.left_arm_ik, self.left_fingers,
@@ -152,7 +164,11 @@ class RetargetSettings(RetargetBase, PropertyGroup):
 
         return False
 
-    deform_preset: EnumProperty(items=preset_handler.iterate_presets, name="Deformation Bones")
+    deform_preset = StringProperty(name="Deformation Bones", subtype='FILE_NAME', default="--")
+
+    last_used_preset = StringProperty(
+        name="Last used preset", default="", description="Preset from which the settings were loaded from (or saved to).",
+        options={'SKIP_SAVE','HIDDEN'}) # base name, not a full path
 
 
 def register_classes():
